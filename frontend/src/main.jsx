ba2458098001e39fb5a5967ad636ba604ed108fa
Samara Souza <samyesouza13@gmail.com>
2024-07-10 17:47:57 -0300

    :sparkles: feat: Layout página de cadastro

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AppRoutes from './routes'
 import {ChakraProvider} from "@chakra-ui/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
-    <App />
+    <AppRoutes />
   </ChakraProvider>,
 )
