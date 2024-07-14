668b2e51ed3e33ecb56587a1f26a6b9890e4912d
Samara Souza <samyesouza13@gmail.com>
2024-07-13 22:07:23 -0300

    :sparkles: feat: Adicionando rota da página de visualizar agendamentos

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Agendamento from "./pages/CadastroAgendamento";
+import VisuAgendamentos from "./pages/VisualizarAgendamentos"
 
 const AppRoutes = () => (
   <>
     <BrowserRouter>
       <Routes>
         <Route element={<Agendamento />} path="/agendar" />
+        <Route element={<VisuAgendamentos />} path="/agendamentos" />
       </Routes>
     </BrowserRouter>
   </>
