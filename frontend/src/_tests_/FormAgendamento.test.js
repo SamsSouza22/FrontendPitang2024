c6f3106b969d452a7ecfca4f6d823714b25fd555
Samara Souza <samyesouza13@gmail.com>
2024-07-15 23:52:36 -0300

    :test_tube: test: Criando teste de validação dos campos do formulário

--- frontend/src/_tests_/FormAgendamento.test.js
+++ frontend/src/_tests_/FormAgendamento.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import ModalProvider from '../contexts/ModalProvider';
 import FormAgendamento from '../components/FormAgendamento';
@@ -17,5 +17,13 @@ test('renderiza o componente FormAgendamento', () => {
     expect(screen.getByLabelText(/Nome/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Data de Nascimento/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Data de Agendamento/i)).toBeInTheDocument();
+});
+
+test('valida os campos do formulário e exibe mensagens de erro', async () => {
+    renderWithProviders(<FormAgendamento />);
+    fireEvent.submit(screen.getByRole('button', { name: /Agendar/i }));
+    expect(await screen.findByText(/Informe seu nome/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Informe a data de nascimento/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Informe a data de agendamento/i)).toBeInTheDocument();
 });
 
