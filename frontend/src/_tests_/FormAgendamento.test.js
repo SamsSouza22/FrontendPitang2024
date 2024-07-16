54865602cfdf77a959b9f27509ea7d4a7f4fdc8e
Samara Souza <samyesouza13@gmail.com>
2024-07-15 23:43:01 -0300

    :test_tube: test: Criando teste de renderizar componente FormAgendamento

--- frontend/src/_tests_/FormAgendamento.test.js
+++ frontend/src/_tests_/FormAgendamento.test.js
@@ -1,6 +1,7 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import ModalProvider from '../contexts/ModalProvider';
+import FormAgendamento from '../components/FormAgendamento';
 
 const renderWithProviders = (ui, providerProps = {}, renderOptions = {}) => {
     return render(
@@ -9,5 +10,12 @@ const renderWithProviders = (ui, providerProps = {}, renderOptions = {}) => {
         </ChakraProvider>,
         renderOptions
     );
-
 };
+
+test('renderiza o componente FormAgendamento', () => {
+    renderWithProviders(<FormAgendamento />);
+    expect(screen.getByLabelText(/Nome/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Data de Nascimento/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Data de Agendamento/i)).toBeInTheDocument();
+});
+
