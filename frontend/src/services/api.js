30831481d83789a69541680075e955ef2bf0fb39
Samara Souza <samyesouza13@gmail.com>
2024-07-17 20:10:03 -0300

    :sparkles: feat: Funções de chamada das rotas no backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -20,4 +20,31 @@ async function fetcher(url, options = {}) {
     }
   }
 
+  fetcher.post = function (url, data) {
+    return fetcher(url, {
+      method: 'POST',
+      data,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
+  fetcher.get = function (url, params) {
+    return fetcher(url, {
+      method: 'GET',
+      params,
+    });
+  };
+
+  fetcher.patch = function (url, data) {
+    return fetcher(url, {
+      method: 'PATCH',
+      data,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
   export default fetcher;
