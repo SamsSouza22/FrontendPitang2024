4253bc4bf3fcc4706b1b007e9f7d44d668d56000
Samara Souza <samyesouza13@gmail.com>
2024-07-17 23:49:03 -0300

    :lipstick: feat: Estilizando NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,40 +1,44 @@
-import {
-    Flex,
-    Heading,
-    Spacer,
-    HStack,
-    Link,
-  } from "@chakra-ui/react";
-  import { Link as RouterLink } from "react-router-dom";
+import { Flex, Heading, Spacer, HStack, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
-  const NavBar = () => {
-  
-    return (
-      <Flex
-        as="nav"
-        p="20px"
-        alignItems="center"
-        bg="#f7af9d"
-        top="0"
-        width="100%"
-        zIndex="1000"
-      >
-        <HStack spacing="20px">
-          <Heading as="h2" size="md">
-            <Link as={RouterLink} to="/" _hover={{ textDecoration: "none" }}>
-              Cadastrar Agendamento
-            </Link>
-          </Heading>
-          <Heading as="h2" size="md">
-            <Link as={RouterLink} to="/agendamentos" _hover={{ textDecoration: "none" }}>
-              Visualizar Agendamentos
-            </Link>
-          </Heading>
-        </HStack>
-        <Spacer />
-      </Flex>
-    );
-  };
-  
-  export default NavBar;
-  
+const NavBar = () => {
+  return (
+    <Flex
+      as="nav"
+      p="20px"
+      alignItems="center"
+      bg="#C8B6FF"
+      boxShadow="md"
+      top="0"
+      width="100%"
+      zIndex="1000"
+      position="sticky"
+    >
+      <HStack spacing="30px">
+        <Heading as="h2" size="md" color="black">
+          <Link
+            as={RouterLink}
+            to="/"
+            _hover={{ textDecoration: "none", color: "gray.600" }}
+            transition="color 0.2s"
+          >
+            Cadastrar Agendamento
+          </Link>
+        </Heading>
+        <Heading as="h2" size="md" color="black">
+          <Link
+            as={RouterLink}
+            to="/agendamentos"
+            _hover={{ textDecoration: "none", color: "gray.600" }}
+            transition="color 0.2s"
+          >
+            Visualizar Agendamentos
+          </Link>
+        </Heading>
+      </HStack>
+      <Spacer />
+    </Flex>
+  );
+};
+
+export default NavBar;
