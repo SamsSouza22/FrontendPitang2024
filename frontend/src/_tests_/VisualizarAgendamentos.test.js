88c5f01cebd2a3ded322044a5f4803f51d98dc19
Samara Souza <samyesouza13@gmail.com>
2024-07-17 21:47:04 -0300

    :test_tube: test: Teste de filtro de agendamentos ao pesquisar

--- frontend/src/_tests_/VisualizarAgendamentos.test.js
+++ frontend/src/_tests_/VisualizarAgendamentos.test.js
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import VisualizarAgendamentos from "../pages/VisualizarAgendamentos";
 import fetcher from "../services/api";
 
@@ -6,11 +6,22 @@ import fetcher from "../services/api";
 jest.mock("../services/api");
 
 const mockAgendamentos = [
-  { id: "1", nome: "João Silva", nascData: "1990-01-01T00:00:00Z", agendData: "2023-07-20T15:30:00Z", status: "Pendente" },
-  { id: "2", nome: "Maria Oliveira", nascData: "1985-05-15T00:00:00Z", agendData: "2023-07-21T14:00:00Z", status: "Pendente" },
+    { id: "1", nome: "João Silva", nascData: "1990-01-01T00:00:00Z", agendData: "2023-07-20T15:30:00Z", status: "Pendente" },
+    { id: "2", nome: "Maria Oliveira", nascData: "1985-05-15T00:00:00Z", agendData: "2023-07-21T14:00:00Z", status: "Pendente" },
 ];
 
 test("deve renderizar o componente e buscar agendamentos", async () => {
+    fetcher.get.mockResolvedValueOnce(mockAgendamentos);
+
+    render(<VisualizarAgendamentos />);
+
+    await waitFor(() => {
+        expect(screen.getByText("João Silva")).toBeInTheDocument();
+        expect(screen.getByText("Maria Oliveira")).toBeInTheDocument();
+    });
+});
+
+test("deve filtrar agendamentos ao pesquisar", async () => {
     fetcher.get.mockResolvedValueOnce(mockAgendamentos);
     
     render(<VisualizarAgendamentos />);
@@ -19,4 +30,10 @@ test("deve renderizar o componente e buscar agendamentos", async () => {
       expect(screen.getByText("João Silva")).toBeInTheDocument();
       expect(screen.getByText("Maria Oliveira")).toBeInTheDocument();
     });
-  });
+    
+    const searchInput = screen.getByPlaceholderText("Pesquisar Agendamento");
+    fireEvent.change(searchInput, { target: { value: "João" } });
+    
+    expect(screen.getByText("João Silva")).toBeInTheDocument();
+    expect(screen.queryByText("Maria Oliveira")).not.toBeInTheDocument();
+});
